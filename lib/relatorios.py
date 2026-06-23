185e22fd9fa10eb9fe2ca190ede403e6f75b5824
Leonardo Comerlato <comerlatoleonardo@gmail.com>
2026-06-22 21:14:09 -0300

    Chore: Pequenos ajustes nos botões relatórios

--- lib/relatorios.py
+++ lib/relatorios.py
@@ -28,7 +28,7 @@ def abrir_menu_relatorios(container: tk.Frame):
 
 def _relatorio_faturamento_geral(container: tk.Frame):
     ui.limpar_container(container)
-    ui.frame_cabecalho(container, "⬛  FATURAMENTO GERAL ENTRE DATAS")
+    ui.frame_cabecalho(container, "FATURAMENTO GERAL ENTRE DATAS")
 
     frame = ui.frame_conteudo(container)
 
@@ -91,7 +91,7 @@ def _relatorio_faturamento_geral(container: tk.Frame):
     fr_btn = tk.Frame(frame, bg=ui.COR_FUNDO)
     fr_btn.pack(pady=4)
     ui.botao_acao(fr_btn, "📊  Gerar", gerar, bg="#1D772A", hover_bg="#329C42").pack(side="left", padx=6)
-    ui.botao_acao(fr_btn, "❌ Fechar", lambda: renderizar_menu_principal(container), bg="#45475a").pack(side="left", padx=6)
+    ui.botao_acao(fr_btn, "⬅️ VOLTAR", lambda: renderizar_menu_principal(container), bg="#45475a").pack(side="left", padx=6)
 
 #  RELATÓRIO 2 — Faturamento por tipo de produto
 
@@ -187,4 +187,4 @@ def _relatorio_por_tipo(container: tk.Frame):
     fr_btn = tk.Frame(frame, bg=ui.COR_FUNDO)
     fr_btn.pack(pady=4)
     ui.botao_acao(fr_btn, "📊  Gerar", gerar, bg="#1D772A", hover_bg="#329C42").pack(side="left", padx=6)
-    ui.botao_acao(fr_btn, "❌ Fechar", lambda: renderizar_menu_principal(container), bg="#45475a").pack(side="left", padx=6)
+    ui.botao_acao(fr_btn, "⬅️ VOLTAR", lambda: renderizar_menu_principal(container), bg="#45475a").pack(side="left", padx=6)
